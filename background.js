e54277131f663375b0a88c06c6edbb13e1e53600
Apurva Singh <101448367+apuurvvaa@users.noreply.github.com>
2022-07-14 16:55:46 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,19 @@
 //listen for messages
 
-chrome.runtime.onMessgae.addListener((msg, sender, responder) =>{
-    if(msg.name=="messgae"){
+chrome.runtime.onMessage.addListener(msg, sender, response) 
+{
+    if(msg.name == "FetchWord"){
 
-        //send response
-        Response({text: "This is a response..."});
+        const apiKey = '99b3ow8205dp464m7b6ypj59s1x1712zcoaro5877bffy4cxk'
+        const dateToday = new Date().toISOString().slice(0, 10); //to only use first ten characters of the string 
+        const apiCall = 'http://api.wordnik.com/v4/words.json/Word_of_the_day?date'+dateToday+'&api_key='+apiKey;
+        
+
+
+//send respond
+        response({api: apiCall, date: dateToday})
     
 
+        
     }
-
-
-
-});
+};
