47b46bc5332ab0649a13f3903300610cbffd65aa
Apurva Singh <101448367+apuurvvaa@users.noreply.github.com>
2022-07-14 16:59:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-//send message to background
+//send messages to the background
 
-chrome.runtime.sendMessage({name: "fetchWords"}, (response) => {
-    //wait for response 
-
-    const apiKey = 'OUR-API-KEY';
-    const dateStr = new Date().toISOString().slice(0, 10);//
-    const apiCall = 'https://api.wordnik.com/v4/words.json/wordOfTheDay?date='+dateStr+'&api_key='+apiKey;
-    //call api..
+chrome.runtime.sendMessage({name: "FetchWord"}, (response))
+{
     //wait for response
-    //send the response
-
+   
+    console.log(response)
+    document.querySelector('h1').innerHTML = response.word;
+        document.querySelector('h1').innerHTML = response.word;
     //update display on content script
-    document.querySelector('body').innerHTML=response.text;
 
-});
+
+   
+}
